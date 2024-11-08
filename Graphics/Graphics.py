d936f3036e881be71dcb5241c97455ff4cabd351
Salnikov Dmitry <dima.salnikov.2014@inbox.ru>
2024-11-08 18:40:49 +0700

    finished project

--- Graphics/Graphics.py
+++ Graphics/Graphics.py
@@ -1,24 +1,85 @@
-import matplotlib.pyplot as plt
-import sys
+# import matplotlib.pyplot as plt
+# import sys
 
-def read_data(file_path):
-    with open(file_path, 'r') as file:
-        data = file.readlines()
+# def read_data(file_path):
+#     with open(file_path, 'r') as file:
+#         data = file.readlines()
     
-    x = []
-    y = []
-    for line in data:
-        values = line.split()
-        x.append(float(values[0]))
-        y.append(float(values[1]))
-    return x,y
+#     x = []
+#     y = []
+#     for line in data:
+#         values = line.split()
+#         x.append(float(values[0]))
+#         y.append(float(values[1]))
+#     return x,y
 
-file_path = '../out.txt'
-x,y = read_data(file_path)
+# file_path = '../out.txt'
+# x,y = read_data(file_path)
 
-plt.scatter(x, y)
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
-plt.title('Grapfics')
+# plt.scatter(x, y)
+# plt.xlabel('X-axis')
+# plt.ylabel('Y-axis')
+# plt.title('Grapfics')
+# plt.grid(True)
+# plt.show()
+
+from cProfile import label
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as sts
+
+from numpy import sign
+from math import pi, exp
+
+NU = 0.05
+K = 1.398
+
+def f(sprd: np.ndarray, nu=NU, k=K) -> np.ndarray:
+    flist = []
+    for x in sprd:
+        if abs(x) <= k:
+            flist.append((1 - nu) / (2 * pi) ** (1 / 2) * exp(-x ** 2 / 2))
+        else:
+            flist.append((1 - nu) / (2 * pi) ** (1 / 2) * exp(1 / 2 * k ** 2- k * abs(x)))
+    return np.array(flist)
+
+def IF_mean(y: np.ndarray, theta):
+    return y - theta
+
+def IF_median(y: np.ndarray, theta, lamb):
+    return lamb * sign(y - theta) / (2 * f(np.array([0]), NU, K)[0])
+
+def IF_truncated_mean(y: np.ndarray, theta, lamb, alpha):
+    k = {
+            0.05 : 2.011802293672519, 
+            0.1  : 1.5032469544798932, 
+            0.15 : 1.1445077882604557
+        }[alpha]
+    
+    res = []
+    for p in y:
+        if (p - theta) / lamb <= -k:
+            res.append(-k)
+        elif abs((p - theta) / lamb) <= k:
+            res.append((p - theta) / lamb)
+            
+        else:
+            res.append(k)
+    return np.array(res)
+
+x = np.linspace(-6, 6, 1000)
+fig, ax = plt.subplots(figsize=(15, 10))
+ax.plot(x, IF_mean(x, 0), label='Mean')
+ax.plot(x, IF_median(x, 0, 1), label='Median')
+ax.plot(x, IF_truncated_mean(x, 0, 1, 0.05), "--", label='Truncated mean 5%')
+ax.plot(x, IF_truncated_mean(x, 0, 1, 0.1), "--", label='Truncated mean 10%')
+ax.plot(x, IF_truncated_mean(x, 0, 1, 0.15), "--", label='Truncated mean 15%') 
 plt.grid(True)
+plt.legend()
+
 plt.show()
+
+
+# q 5% 2.011802293672519
+# q 10% 1.5032469544798932
+# q 15% 1.1445077882604557
--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ namespace SPPMiNPO;
 public class Program
 {
     private static readonly string OutPath = "../../../out.txt";
+    private static int N = 500;
+
+    private const double variableAlpha = 0.20d;
+    private const double alpha1 = 0.05d;
+    private const double alpha2 = 0.10d;
+    private const double alpha3 = 0.15d;
 
     static void Main()
     {
@@ -21,10 +27,62 @@ public class Program
             P = 0.796
         });
 
+        var values = distribution.GenerateValues(N);
+
+        var quantiles = CalcQuantiles(values);
+
+        Console.WriteLine($"Квантиль 5% {quantiles.Item1}\n" +
+                          $"Квантиль 10% {quantiles.Item2}\n" +
+                          $"Квантиль 15% {quantiles.Item3}\n");
+
+
+        // PrintArithmeticParameters(values);
+        // WriteDataToFile(values);
+    }
+
+    private static Tuple<double, double, double> CalcQuantiles(double[] values)
+    {
+        var sortedValues = values.OrderBy(x => x).ToArray();
+
+        var q1 = sortedValues[(int)((1 - alpha1) * N) + 1];
+        var q2 = sortedValues[(int)((1 - alpha2) * N) + 1];
+        var q3 = sortedValues[(int)((1 - alpha3) * N) + 1];
+
+        return (q1, q2, q3).ToTuple();
+    }
+
+    private static void PrintArithmeticParameters(double[] values)
+    {
+        var mean = StatisticalLibrary.Mean(values);
+        var median = StatisticalLibrary.Median(values);
+        var variance = StatisticalLibrary.Variance(values);
+        var coefficientAsymmetry = StatisticalLibrary.CoefficientAsymmetry(values);
+        var coefficientExcess = StatisticalLibrary.CoefficientExcess(values);
+        var meanCut = StatisticalLibrary.MeanCut(values, variableAlpha);
+
+        var meanCut1 = StatisticalLibrary.MeanCut(values, alpha1);
+        var meanCut2 = StatisticalLibrary.MeanCut(values, alpha2);
+        var meanCut3 = StatisticalLibrary.MeanCut(values, alpha3);
+
+        Console.WriteLine(
+            $"Среднее арифметическое {mean:F7}\n" +
+            $"Медиана {median:F7}\n" +
+            $"Дисперсия {variance:F7}\n" +
+            $"Коэффициент асимметрии {coefficientAsymmetry:F7}\n" +
+            $"Коэффициент эксцесса {coefficientExcess:F7}\n" +
+            $"Усечённое среднее 20% {meanCut:F7}\n" +
+            $"Усечённое среднее 5% {meanCut1:F7}\n" +
+            $"Усечённое среднее 10% {meanCut2:F7}\n" +
+            $"Усечённое среднее 15% {meanCut3:F7}\n"
+            );
+    }
+
+    private static void WriteDataToFile(double[] values)
+    {
         using var writer = new StreamWriter(OutPath);
 
         int i = 1;
-        foreach (var item in distribution.GenerateValues(500))
+        foreach (var item in values)
         {
             writer.WriteLine($"{i} {item}");
             ++i;
